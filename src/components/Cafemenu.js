fda5cde18ac95cba3f95d6141356408a87bea841
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2024-08-17 23:08:47 -0400

    modal feature updated

--- src/components/Cafemenu.js
+++ src/components/Cafemenu.js
@@ -1,10 +1,17 @@
 import americano from "../image/Americano.png";
+import icedAmericano from "../image/IcedAmericano.png";
 import cafelatte from "../image/cafelatte.png";
+import IcedLatte from "../image/IcedLatte.png";
 import cafemocha from "../image/cafemocha.png";
+import IcedMocha from "../image/IcedMocha.png";
 import whitemocha from "../image/whitemocha.png";
+import IcedWhiteMocha from "../image/IcedWhiteMocha.png";
 import caramelmacchiato from "../image/caramelmacchiato.png";
+import IcedCaramelMacchiato from "../image/IcedCaramelMacchiato.png";
 import vanillalatte from "../image/vanillalatte.png";
+import IcedVanillaLatte from "../image/IcedVanillaLatte.png";
 import handdrip from "../image/handdrip.png";
+import IcedHandDrip from "../image/IcedHandDrip.png";
 import affogato from "../image/affogato.png";
 
 export const cafemenu = [
@@ -13,18 +20,21 @@ export const cafemenu = [
     drinkname: "AMERICANO",
     ingredients: ["espresso 2 shots", "water"],
     image: americano,
+    icedimage: icedAmericano,
   },
   {
     id: "cafelatte",
     drinkname: "CAFE LATTE",
     ingredients: ["espresso 2 shots", "milk"],
     image: cafelatte,
+    icedimage: IcedLatte,
   },
   {
     id: "cafemocha",
     drinkname: "CAFE MOCHA",
     ingredients: ["espresso 2 shots", "chocolate syrup", "milk"],
     image: cafemocha,
+    icedimage: IcedMocha,
   },
 
   {
@@ -32,29 +42,34 @@ export const cafemenu = [
     drinkname: "CARAMEL MACCHIATO",
     ingredients: ["espresso 2 shots", "caramel sauce", "vanilla syrup", "milk"],
     image: caramelmacchiato,
+    icedimage: IcedCaramelMacchiato,
   },
   {
     id: "vanillalatte",
     drinkname: "VANILLA LATTE",
     ingredients: ["espresso 2 shots", "vanilla syrup", "milk"],
     image: vanillalatte,
+    icedimage: IcedVanillaLatte,
   },
   {
     id: "whitemocha",
     drinkname: "WHITE MOCHA",
     ingredients: ["espresso 2 shots", "white chocolate syrup", "milk"],
     image: whitemocha,
+    icedimage: IcedWhiteMocha,
   },
   {
     id: "handdrip",
     drinkname: "HAND DRIP",
     ingredients: [""],
     image: handdrip,
+    icedimage: IcedHandDrip,
   },
   {
     id: "affogato",
     drinkname: "AFFOGATO",
     ingredients: ["espresso 2 shots", "ice cream scoops"],
     image: affogato,
+    icedimage: icedAmericano,
   },
 ];
--- src/components/Churista.js
+++ src/components/Churista.js
@@ -1,10 +1,21 @@
 import churista from "../image/churista.gif";
 import "./Churista.css";
 import { cafemenu } from "./Cafemenu";
+import { useState, useEffect } from "react";
+import Modal from "react-bootstrap/Modal";
+import CloseButton from "react-bootstrap/CloseButton";
+import Button from "react-bootstrap/Button";
+import ToggleButton from "react-bootstrap/ToggleButton";
+import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
 
 const Churista = () => {
+  const [fullscreen, setFullscreen] = useState(true);
+  const [show, setShow] = useState(false);
+  const [currentDrinkData, setCurrentDrinkData] = useState([]);
+  const [IsIced, setIsIced] = useState(false);
+
   const listItems = cafemenu.map((drink) => (
-    <div>
+    <div onClick={() => handleOnClick(drink)}>
       <div className="block" id={drink.id}>
         <img
           src={drink.image}
@@ -19,10 +30,90 @@ const Churista = () => {
     </div>
   ));
 
+  function handleShow() {
+    if (show) {
+      setFullscreen(false);
+      setShow(false);
+      setIsIced(false);
+    } else {
+      setFullscreen(true);
+      setShow(true);
+    }
+  }
+
+  function handleOnClick(drink) {
+    handleShow();
+    setCurrentDrinkData({ ...drink });
+  }
+
+  const handleIsIcedOnClick = (value) => {
+    setIsIced(value);
+  };
+
+  useEffect((value) => {
+    handleIsIcedOnClick(value);
+  }, []);
+
   return (
     <div>
       <img src={churista} alt="" className="neonsign"></img>
-      {listItems}
+      <div>{listItems}</div>
+      <Modal
+        show={show}
+        fullscreen={fullscreen}
+        onHide={() => setShow(false)}
+        id={currentDrinkData.id}
+      >
+        <Modal.Body>
+          <CloseButton variant="white" onClick={() => handleShow()} />
+          <div>
+            {!IsIced && (
+              <img
+                src={currentDrinkData.image}
+                alt=""
+                className="modal-coffee-image"
+              ></img>
+            )}
+            {IsIced && (
+              <img
+                src={currentDrinkData.icedimage}
+                alt=""
+                className="modal-iced-coffee-image"
+              ></img>
+            )}
+
+            <div className="drink-name">{currentDrinkData.drinkname}</div>
+            <div>
+              <div className="Button">
+                <ToggleButtonGroup
+                  type="radio"
+                  name="options"
+                  defaultValue={false}
+                >
+                  <ToggleButton
+                    id="hot"
+                    value={false}
+                    className={IsIced ? "boxshadow" : ""}
+                    onClick={() => handleIsIcedOnClick(false)}
+                  >
+                    Hot
+                  </ToggleButton>
+                  <ToggleButton
+                    id="iced"
+                    style={{ marginLeft: "5rem" }}
+                    value={true}
+                    className={IsIced ? "" : "boxshadow"}
+                    onClick={() => handleIsIcedOnClick(true)}
+                  >
+                    Iced
+                  </ToggleButton>
+                </ToggleButtonGroup>
+                <Button className="order-button boxshadow">Place order</Button>
+              </div>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 };
